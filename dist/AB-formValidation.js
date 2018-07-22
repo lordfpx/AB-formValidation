63892bbe671d26a79b6600abc66024e2a687e63c
Thierry PHILIPPE <thierryphilippe@Macbook-Pro-de-Thierry.local>
2018-07-22 21:22:10 +0200

    Add missing updated build

--- dist/AB-formValidation.js
+++ dist/AB-formValidation.js
@@ -184,7 +184,7 @@ FormValidation.prototype = {
     // prepare form
     this.el.setAttribute('novalidate', 'novalidate');
     this.submitBtn.classList.add(this.settings.classBtnDisabled);
-
+    this.submitBtn.setAttribute("disabled", "disabled");
     this._events();
   },
 
@@ -232,12 +232,14 @@ FormValidation.prototype = {
 
   setValid: function() {
     this.submitBtn.classList.remove(this.settings.classBtnDisabled);
+    this.submitBtn.removeAttribute("disabled");
     this.el.classList.remove(this.settings.classInvalid);
     this.el.classList.add(this.settings.classValid);
   },
 
   setInvalid: function() {
     this.submitBtn.classList.add(this.settings.classBtnDisabled);
+    this.submitBtn.addAttribute("disabled", "disabled");
     this.el.classList.add(this.settings.classInvalid);
     this.el.classList.remove(this.settings.classValid);
   }
